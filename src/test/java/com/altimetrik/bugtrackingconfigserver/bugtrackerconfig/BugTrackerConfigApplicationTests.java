9a78ad82afec67e4f7bb5d22d87d4920fa526310
Deeptirekha Priyadarsini <dpriyadarsini@altimetrik.com>
2019-11-17 19:38:27 +0530

    Adding base

---
+++ src/test/java/com/altimetrik/bugtrackingconfigserver/bugtrackerconfig/BugTrackerConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altimetrik.bugtrackingconfigserver.bugtrackerconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugTrackerConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
