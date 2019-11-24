2739d4eb2db2aae6f04fef2d72e2cb4da9803a01
Deeptirekha Priyadarsini <dpriyadarsini@altimetrik.com>
2019-11-24 13:21:25 +0530

    change in tracker config module

--- src/main/java/com/altimetrik/bugtrackingconfigserver/bugtrackerconfig/BugTrackerConfigApplication.java
+++ src/main/java/com/altimetrik/bugtrackingconfigserver/bugtrackerconfig/BugTrackerConfigApplication.java
@@ -3,6 +3,9 @@ package com.altimetrik.bugtrackingconfigserver.bugtrackerconfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
+import org.h2.tools.Server;
+import java.sql.SQLException;
 
 @EnableConfigServer
 @SpringBootApplication
@@ -11,5 +14,9 @@ public class BugTrackerConfigApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BugTrackerConfigApplication.class, args);
 	}
-
+	@Bean(initMethod = "start", destroyMethod = "stop")
+	public Server inMemoryH2DatabaseaServer() throws SQLException {
+		return Server.createTcpServer(
+				"-tcp", "-tcpAllowOthers", "-tcpPort", "9090");
+	}
 }
